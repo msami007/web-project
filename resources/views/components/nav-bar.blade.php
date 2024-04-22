87e9356b81282b421e344ec661192b024e0ca83b
Muhammad Sami <saisami902@gmail.com>
2024-04-22 17:21:12 +0500

    testing

---
+++ resources/views/components/nav-bar.blade.php
@@ -0,0 +1,26 @@
+<div>
+    <div class="container-fluid bg-dark">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/')}}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/register')}}">Register</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/customer')}}">Customer</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+    
+</div>
