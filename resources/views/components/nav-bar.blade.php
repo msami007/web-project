0f08aade9addd467fa9ab76d842f4d7439009a56
Muhammad Sami <saisami902@gmail.com>
2024-04-22 18:50:27 +0500

    1st

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,23 +1,14 @@
 <div>
-    <div class="container-fluid bg-dark">
-        <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid bg-light">
+        <div class="fluid-container mx-3">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
                 <a class="navbar-brand" href="#">Navbar</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{url('/')}}">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{url('/register')}}">Register</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{url('/customer')}}">Customer</a>
-                        </li>
-                    </ul>
+                <div class="collapse navbar-collapse navbars" id="navbarNav">
+                    <button type="button" class="btn btn-outline-primary m-2">Sign in</button>
+                    <button class="btn btn-primary" type="submit">Sign up</button>
                 </div>
             </nav>
         </div>
